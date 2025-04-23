500513d445e89a0bc638f48d79cf943a83902679
mayd-diallo <maydiallo78@gmail.com>
2025-04-23 11:56:41 +0200

    bonjour

--- java.java
+++ java.java
@@ -1,15 +1,20 @@
 /***--------Donner un nom à votre class -------------------****/
-
+import java.util.Scanner;
 public class java{
 
 
     public static void main(String[] args) {
 
           /***--------  Votre code ici  -------------------****/
-  
-          System.out.print("Bonne vacance");
+          Scanner sc = new Scanner(System.in);
+          System.out.print("Bonne vacance ");
 
-    }System.out.print("Bonne vacance");
+          String name = sc.nextLine();
+          System.out.print("prenom=" + name); 
+
+
+
+    }
     
 
 }
