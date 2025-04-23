ca7b584b6454a5ac98f0641712745d00d2c44ce9
mayd-diallo <maydiallo78@gmail.com>
2025-04-23 08:55:40 +0200

    test

---
+++ test.java
@@ -0,0 +1,14 @@
+/***--------Donner un nom à votre class -------------------****/
+
+public class test{
+
+
+    public static void main(String[] args) {
+
+          /***--------  Votre code ici  -------------------****/
+  
+
+
+    }
+
+}
