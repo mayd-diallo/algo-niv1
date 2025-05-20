e5c73637093a779e3c6232aef89cc24e9042e8d5
mayd-diallo <maydiallo78@gmail.com>
2025-05-20 17:00:55 +0200

    jalon

--- exercices/jalonalgo/jalon.java
+++ exercices/jalonalgo/jalon.java
@@ -1,5 +1,21 @@
-package jalonalgo;
+import java.utile.Scanner;
 
 public class jalon {
     
+}public static void main(String[] args){
+    Scanner scanner = new scanner(String.in);
+    final int bouteuillelitre = 1  ;
+    int capacitébouteuillecl = 100 ;
+    System.out.println("combien de cl d'eua voulez-vous boire ! ");
+    int contenuebouteuille =1=100 ;
+    if (contenuebouteuille==0){
+        System.out.println("la bouteuille est vide");
+        else if (contenuebouteuille>0){
+            System.out.println("la bouteuille contient de l'eau");
+            else {
+                System.out.println("la bouteuile contient 1l (100cl)");
+
+            }
+        } Scanner.close();
+    }
 }
